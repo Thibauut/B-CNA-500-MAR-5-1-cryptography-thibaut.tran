aaa0967cb665aa01d59896eabb13fe8fdc170707
TranThibaut <thibaut.tran@epitech.eu>
2023-11-16 19:24:08 +0000

    new algo

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <gmpxx.h>
 
-// Function to calculate modular exponentiation (base^exp mod mod)
 mpz_class mod_pow(mpz_class base, mpz_class exp, mpz_class mod) {
     mpz_class result = 1;
     while (exp > 0) {
@@ -43,10 +42,10 @@ mpz_class mod_inverse(mpz_class a, mpz_class m) {
 
 
 int main() {
-    // std::string a1 = "d3";
-    // std::string a2 = "e3";
-    std::string a1 = "4b1da73924978f2e9c1f04170e46820d648edbee12ccf4d4462af89b080c86e1";
-    std::string a2 = "bb3ca1e126f7c8751bd81bc8daa226494efb3d128f72ed9f6cacbe96e14166cb";
+    std::string a1 = "d3";
+    std::string a2 = "e3";
+    // std::string a1 = "4b1da73924978f2e9c1f04170e46820d648edbee12ccf4d4462af89b080c86e1";
+    // std::string a2 = "bb3ca1e126f7c8751bd81bc8daa226494efb3d128f72ed9f6cacbe96e14166cb";
 
     mpz_class decimal;
     mpz_set_str(decimal.get_mpz_t(), a1.c_str(), 16);
@@ -54,26 +53,37 @@ int main() {
     mpz_class decimal2;
     mpz_set_str(decimal2.get_mpz_t(), a2.c_str(), 16);
 
-
     mpz_class p = decimal;
     mpz_class q = decimal2;
-    std::cout << "p: " << p << std::endl;
-    std::cout << "q: " << q << std::endl;
+    // std::cout << "p: " << p << std::endl;
+    // std::cout << "q: " << q << std::endl;
 
     mpz_class n = p * q;
 
-    std::cout << "n: " << n << std::endl;
-
     mpz_class totient = (p - 1) * (q - 1);
-    mpz_class e = 65537; // Public exponent
-    mpz_class d = mod_inverse(e, totient); // Private exponent
+    mpz_class e = 65537;
+    mpz_class d = mod_inverse(e, totient);
 
-    // // Display keys in hex
-    std::cout << "Public Key: "<< "0" << std::hex << e << "-" << std::hex << n  << std::endl;
-    std::cout << "Private Key: " << std::hex << d << "-" << n << std::endl;
+    std::string e_str = e.get_str(16);
+    std::string n_str = n.get_str(16);
+    std::string d_str = d.get_str(16);
 
-    // std::cout << "Public Key (n, e): (" << n << ", " << e << ")" << std::endl;
-    // std::cout << "Private Key (n, d): (" << n << ", " << d << ")" << std::endl;
+
+    std::string test1 = "bb";
+    std::string test2 = "19";
+
+    mpz_class test;
+    mpz_set_str(test.get_mpz_t(), test1.c_str(), 16);
+
+    mpz_class test3;
+    mpz_set_str(test3.get_mpz_t(), test2.c_str(), 16);
+
+    std::cout << "test: " << mpz_sizeinbase(test.get_mpz_t(), 2) << " bits" << std::endl;
+    std::cout << "test3: " << mpz_sizeinbase(test3.get_mpz_t(), 2) << " bits" << std::endl;
+
+
+    std::cout << "Public Key: " << e_str << "-" << n_str << std::endl;
+    std::cout << "Private Key: " << d_str << "-" << n_str << std::endl;
 
     return 0;
 }
